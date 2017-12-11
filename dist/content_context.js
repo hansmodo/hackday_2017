bf235ec0a68e37b2a8707802ef5c71825aa849d8
Hans Brough <hans@edmodo.com>
2017-12-11 08:10:24 -0800

    add a unique count to chrome badge on page load.

--- dist/content_context.js
+++ dist/content_context.js
@@ -8,6 +8,8 @@
   const RE_IMG_URL      = /(https?:\/\/.*\.(?:png|jpg|jpeg|gif))/;
   const MAX_IMG_WIDTH   = 260;
 
+  let people = [];
+
   /*
   * return list of data objects describing page.
   */
@@ -57,10 +59,11 @@
   /*
   * recursively walk through a given node looking for text.
   * ignore whitespace textnodes
+  * aggregator arg should be array. keeps value between recursive calls
   * e.g. document.body
   */
-  function findTextInNode(node, regex){
-    //console.log("content_context findTextInNode");
+  function findTextInNode(node, regex, aggregator){
+    //console.log("findTextInNode aggregator:",aggregator);
     let nodeNameList = ['div','p','section','article'];
     let childNodes = Array.from(node.childNodes);
     childNodes.forEach(function(child){
@@ -68,10 +71,13 @@
       let nodeName = child.nodeName.toLowerCase();
       if( nodeNameList.includes(nodeName) ){
         //console.log(child.nodeName.toLowerCase());
-        findTextInNode(child,regex);
+        findTextInNode(child,regex,aggregator);
       }else if(nodeName === '#text' && child.nodeValue.trim()){//ignore whitespace
-
         let matches = getMatchesInStr(child.nodeValue, regex);
+        //console.log("...matches:",matches);
+        //aggregator = aggregator.concat(matches);
+        aggregator.push.apply(aggregator, matches);
+        //console.log("...aggregator:",aggregator);
         if(matches.length){
           //console.log(child," ",child.length);
           decorateTextNode(child, matches);
@@ -141,14 +147,37 @@
   chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
       //console.log('request:',request);
-      if(request.plugin === 'load'){
+      if(request.popup === 'load'){
+        console.log('popup load:',request)
         injectStylesheet(request);
         let data = getData();
-        let peopleRegex = new RegExp(request.regex, 'gi');
+        //let peopleRegex = new RegExp(request.regex, 'gi');
+        //let people = [];
+        //findTextInNode(document.body, peopleRegex, people);
+        //console.log("...people:",people);
+        data.push({'people':people});
         //console.log("...peopleRegex:",peopleRegex);
         sendResponse({'content_data':data});
-        findTextInNode(document.body, peopleRegex);
+
       }
   });
 
+
+  chrome.runtime.sendMessage({ 'content':'load' }, function(response) {
+    console.log("content load response",response);
+    let peopleRegex = new RegExp(response.regex, 'gi');
+    findTextInNode(document.body, peopleRegex, people);
+    console.log("people:",people);
+
+    chrome.runtime.sendMessage({'content':'parsed','people':people});
+  });
+
+
 })();
+
+//document.addEventListener("DOMContentLoaded", function(event) {
+  //console.log('DOMContentLoaded');
+
+//});
+
+//console.log('Content Context');
